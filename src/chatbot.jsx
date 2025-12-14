cb3a1ba699b3e296d9b2f3297eb639a8989bdfd9
ankit-khatkar <ankitofficial1504@gmail.com>
2025-12-14 19:59:08 +0530

    Added UI

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -1,9 +1,54 @@
 import { useState } from "react";
-
+import "./chatbot.css";
+import { CiUser } from "react-icons/ci";
+import { FaBraille } from "react-icons/fa";
+function ChatMsg(props) {
+  const { message, sender, className } = props;
+  if (sender === "user") {
+    return (
+      <>
+        <div className={className}>
+          <p>{message}</p>
+          <CiUser />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className={className}>
+          <FaBraille />
+          <p>{message}</p>
+        </div>
+      </>
+    );
+  }
+}
 function Chatbot() {
   return (
     <>
-      <h3>Hello World</h3>
+      <div className="chatContainor">
+        <div>
+          <input
+            type="text"
+            placeholder="Ask anything to Lotus AI"
+            className="searchBox"
+          />
+          <button className="btn">Send</button>
+        </div>
+        <div>
+          <ChatMsg
+            message="Hii, Lotus AI"
+            sender="user"
+            className="chatUiUser"
+          />
+          <ChatMsg
+            message="Hello Ankit, How can i help you ?"
+            sender="robot"
+            className="chatUiRobot"
+          />
+        </div>
+      </div>
     </>
   );
 }
