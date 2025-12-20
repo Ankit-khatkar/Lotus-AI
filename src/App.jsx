decaa95d772be26e69874d5695e310a2c04c1e04
ankit-khatkar <ankitofficial1504@gmail.com>
2025-12-20 22:23:28 +0530

    clean repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Chatbot from "./chatbot";
-import ProductsWindow from "./Exercises/ecommorsProduct";
-import Chexercise from "./Exercises/exerciseCh03";
-import ExerciseCh04 from "./Exercises/exerciseCh04";
 function App() {
   return (
     <>
       <Chatbot />
-      <ExerciseCh04 />
     </>
   );
 }
